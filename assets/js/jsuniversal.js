3cdd5f3bb47af1ace41f2d43873ae4b93df5d742
IvanEz610 <ivanezquivel672@gmail.com>
2024-12-10 17:12:18 -0300

    Add files via upload

--- assets/js/jsuniversal.js
+++ assets/js/jsuniversal.js
@@ -1,3 +1,34 @@
 function  volverAPaginaAnterior() {
         window.history.back()
     }
+function informacion() {alert ("Programador: Iván Francsico Ezquivel - Edad: 17 años - Nacionalidad: Argentino - Correo: ivanezquivel672@gmail.com")}
+
+
+
+function darkmode() {
+document.body.style.backgroundColor="black"
+document.getElementById("barra").style.backgroundColor="black"
+document.getElementById("inicio").style.backgroundColor="#545454"
+document.getElementById("inicio1").style.backgroundColor="#545454"
+document.getElementById("cuerpo").style.backgroundColor="#2B2B2B"
+document.getElementById("cuerpo1").style.backgroundColor="#2B2B2B"
+document.getElementById("cuerpo2").style.backgroundColor="#2B2B2B"
+
+document.body.style.backgroundColor="black"
+document.getElementById("pbarra").style.backgroundColor="black"
+document.getElementById("pinicio").style.backgroundColor="#545454"
+document.getElementById("pinicio1").style.backgroundColor="#545454"
+document.getElementById("pcuerpo").style.backgroundColor="#2B2B2B"
+document.getElementById("pcuerpo1").style.backgroundColor="#2B2B2B"
+document.getElementById("pcuerpo2").style.backgroundColor="2B2B2B"
+
+
+document.getElementById("").style.backgroundColor=""
+document.getElementById("").style.backgroundColor=""
+
+
+
+
+}
+
+ 
